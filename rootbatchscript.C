55bc63ec8e87cfc0fe83cdf1d3a280926530c633
Stefan Kluth <skluth@mpp.mpg.de>
2016-02-01 17:41:12 +0100

    move to FastJet 3.1.3

--- rootbatchscript.C
+++ rootbatchscript.C
@@ -1,7 +1,7 @@
 {
 gROOT->LoadMacro( "libNtupleReader.so");
 gROOT->LoadMacro( "libNtupleReaderDict.so");
-gROOT->ProcessLine(".include /home/skluth/qcd/fastjet/fastjet-3.0.6/install/include");
+gROOT->ProcessLine(".include /home/skluth/qcd/fastjet/fastjet-3.1.3/install/include");
 gROOT->LoadMacro( "LEP1Analysis.C+" );
 LEP1Analysis(1000);
 // LEP1Analysis();

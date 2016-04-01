387ade0da2198d93bcaa902ca79e31d05824a9a5
Stefan Kluth <skluth@mpp.mpg.de>
2016-04-01 17:11:33 +0200

    Calculator for Ycut based jet rates

---
+++ YcutCalculator.hh
@@ -0,0 +1,22 @@
+#ifndef YCUTCALCULATOR_HH
+#define YCUTCALCULATOR_HH
+
+#include "JetrateCalculator.hh"
+
+class NtupleReader;
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+class YcutCalculator: public JetrateCalculator {
+  string algorithm;
+public:
+  YcutCalculator( const string& );
+  ~YcutCalculator() {}
+  vector<Double_t> getValues( NtupleReader*, 
+			      const vector<Double_t>&,
+			      const string& ) const;
+};
+
+#endif

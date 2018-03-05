ba8601b28a0673d8ef60815dc6df3eff1f50ad55
Stefan Kluth <skluth@mpp.mpg.de>
2018-03-05 16:42:33 +0100

    add LinkDef for libAnalysisDict.so dictionary creation

---
+++ AnalysisLinkDef.h
@@ -0,0 +1,9 @@
+#ifdef __MAKECINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ class Analysis;
+#pragma link C++ class AnalysisObject;
+#pragma link C++ class TH1DAnalysisObject;
+#pragma link C++ class TGEAnalysisObject;
+#endif

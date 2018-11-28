fc3b14c70ddc8d44fef984d57fda129c2472d210
Stefan Kluth <skluth@mpp.mpg.de>
2018-11-28 11:49:32 +0100

    move to AnalysisProcessor::LEPAnalysis(...)

--- runjob.cc
+++ runjob.cc
@@ -7,7 +7,7 @@ int main( int argc, const char *argv[] ) {
   SjmConfigParser sjmcp( argc, argv );
   sjmcp.printConfig();
   AnalysisProcessor ap( sjmcp );
-  ap.LEP1Analysis();
+  ap.LEPAnalysis();
   return 0;
 
 }

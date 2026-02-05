9abc9f59bfd8ee37a70aa49cd2112d0d91c7c8ba
Stefan Kluth <skluth@mpp.mpg.de>
2026-02-05 15:03:41 +0100

    python3 changes

--- SjmConfigParser.py
+++ SjmConfigParser.py
@@ -1,19 +1,24 @@
 
-import ConfigParser
+
+import configparser
 from collections import OrderedDict
 
 # Support multiple entries for same key:
 # From the net somewhere ...
+# https://stackoverflow.com/questions/15848674/how-to-configparse-a-file-keeping-multiple-values-for-identical-keys/15848928#15848928
+
 class MultiOrderedDict( OrderedDict ):
 
     def __setitem__( self, key, value ):
         if key in self and isinstance( value, list ):
             self[key].extend( value )
         else:
-            super( MultiOrderedDict, self ).__setitem__( key, value )
+#            super( MultiOrderedDict, self ).__setitem__( key, value )
+            super().__setitem__( key, value )
+
 
 def readConfig( name="test.cfg" ):
-    config= ConfigParser.ConfigParser( dict_type=MultiOrderedDict )
+    config= configparser.RawConfigParser( dict_type=MultiOrderedDict, strict=False )
     config.read( name )    
     return config
 
--- checkEventSelection.py
+++ checkEventSelection.py
@@ -1,42 +1,34 @@
 
 from ROOT import TChain, TFile
-    
 
 def checkSelection( cfgfile="sjmconfig_192.cfg" ):
-
     import SjmConfigParser
-
     configfiles= [ cfgfile, "sjmGeneralOptions.cfg" ]
     config= SjmConfigParser.readConfig( configfiles )
-
     ecms= config.get( "General", "energy" )
-    print cfgfile
-    
-    testSample( config, "Signal", ecms )
-    testSample( config, "AltSignal", ecms )
-
-    testSample( config, "BkgWWllqq", ecms )
-    testSample( config, "BkgWWqqqq", ecms )
-    testSample( config, "BkgWWeeqq", ecms )
-    
+    path= config.get( "General", "path" )
+    print( cfgfile )
+    testSample( config, "Signal", ecms, path )
+    testSample( config, "AltSignal", ecms, path )
+    testSample( config, "BkgWWllqq", ecms, path )
+    testSample( config, "BkgWWqqqq", ecms, path )
+    testSample( config, "BkgWWeeqq", ecms, path )
     return
 
-    
-def testSample( config, sample, ecms ):
+def testSample( config, sample, ecms, path ):
     filenames= config.get( sample, "files" ).split()
-    print sample, config.get( sample, "name" )
+    print( sample, config.get( sample, "name" ) )
     chain= TChain( "h10" )
     for filename in filenames:
-        print filename
-        chain.Add( filename )
+        print( filename )
+        chain.Add( path+"/"+filename )
     testChain( chain, ecms )
     return
-
         
 def testChain( chain, ecms ):
 
     ecms+= ".0"
-    
+
     nevtStand= chain.Draw( "Tdtc", "Icjst==3&&Iebst==3&&Il2mh==1&&Ntkd02>6&&fabs(Tvectc[2])<0.9&&"+ecms+"-sqrt( fabs( Pspr[3]*Pspr[3]-Pspr[2]*Pspr[2]-Pspr[1]*Pspr[1]-Pspr[0]*Pspr[0] ) )<10.0&&Lwqqln<0.5&&Lwqqqq<0.25" )
     nevtCostt07= chain.Draw( "Tdtc", "Icjst==3&&Iebst==3&&Il2mh==1&&Ntkd02>6&&fabs(Tvectc[2])<0.7&&"+ecms+"-sqrt( fabs( Pspr[3]*Pspr[3]-Pspr[2]*Pspr[2]-Pspr[1]*Pspr[1]-Pspr[0]*Pspr[0] ) )<10.0&&Lwqqln<0.5&&Lwqqqq<0.25" )
     nevtSprold= chain.Draw( "Tdtc", "Icjst==3&&Iebst==3&&Il2mh==1&&Ntkd02>6&&fabs(Tvectc[2])<0.9&&"+ecms+"-sqrt( fabs( Pspri[3]*Pspri[3]-Pspri[2]*Pspri[2]-Pspri[1]*Pspri[1]-Pspri[0]*Pspri[0] ) )<10.0&&Lwqqln<0.5&&Lwqqqq<0.25" )
@@ -45,28 +37,29 @@ def testChain( chain, ecms ):
     nevtqqlnlo= chain.Draw( "Tdtc", "Icjst==3&&Iebst==3&&Il2mh==1&&Ntkd02>6&&fabs(Tvectc[2])<0.9&&"+ecms+"-sqrt( fabs( Pspr[3]*Pspr[3]-Pspr[2]*Pspr[2]-Pspr[1]*Pspr[1]-Pspr[0]*Pspr[0] ) )<10.0&&Lwqqln<0.25&&Lwqqqq<0.25" )
     nevtqqqqhi= chain.Draw( "Tdtc", "Icjst==3&&Iebst==3&&Il2mh==1&&Ntkd02>6&&fabs(Tvectc[2])<0.9&&"+ecms+"-sqrt( fabs( Pspr[3]*Pspr[3]-Pspr[2]*Pspr[2]-Pspr[1]*Pspr[1]-Pspr[0]*Pspr[0] ) )<10.0&&Lwqqln<0.5&&Lwqqqq<0.4" )
     nevtqqqqlo= chain.Draw( "Tdtc", "Icjst==3&&Iebst==3&&Il2mh==1&&Ntkd02>6&&fabs(Tvectc[2])<0.9&&"+ecms+"-sqrt( fabs( Pspr[3]*Pspr[3]-Pspr[2]*Pspr[2]-Pspr[1]*Pspr[1]-Pspr[0]*Pspr[0] ) )<10.0&&Lwqqln<0.5&&Lwqqqq<0.1" )
-    print "Standard", nevtStand
-    print "Costt07", nevtCostt07
-    print "Sprold", nevtSprold
-    print "Wqqlnhi", nevtqqlnhi
-    print "Wqqlnlo", nevtqqlnlo
-    print "Wqqqqhi", nevtqqqqhi
-    print "Wqqqqlo", nevtqqqqlo
+    
+    print( "Standard", nevtStand )
+    print( "Costt07", nevtCostt07 )
+    print( "Sprold", nevtSprold )
+    print( "Wqqlnhi", nevtqqlnhi )
+    print( "Wqqlnlo", nevtqqlnlo )
+    print( "Wqqqqhi", nevtqqqqhi )
+    print( "Wqqqqlo", nevtqqqqlo )
 
     return
 
 
-def countEvents( obs="thrust" ):
+def countEvents( obs="lepthrust" ):
 
     selectionKeys= [ "stand", "costt07", "sprold", "wqqlnhi", "wqqlnlo", "wqqqqhi", "wqqqqlo" ]
     ecms= [ "91", "130", "136", "161", "172", "183", "189", "192", "196",
                 "200", "202", "205", "207" ]
 
-    print "Number of events for ecm (using "+obs+")"
-    print " ecm",
+    print( "Number of events for ecm (using "+obs+")" )
+    print( " ecm", end="" )
     for selectionKey in selectionKeys:
-        print "{:>8s}".format( selectionKey ),
-    print
+        print( "{:>10s}".format( selectionKey ), end="" )
+    print()
 
     for ecm in ecms:
         filename= "sjm"+ecm+".root"
@@ -81,10 +74,10 @@ def countEvents( obs="thrust" ):
                 counters[selectionKey]= hist.GetEntries()
             else:
                 counters[selectionKey]= 0
-        print "{:>4s}".format( ecm ),
+        print( "{:>4s}".format( ecm ), end="" )
         for selectionKey in selectionKeys:
-            print "{:8.1f}".format( counters.get( selectionKey ) ),
-        print
+            print( "{:10.1f}".format( counters.get( selectionKey ) ), end="" )
+        print()
         
     return
 
@@ -99,7 +92,7 @@ def unpackAlphanumericHisto( hist ):
             value= hist.GetBinContent( ibin )
             values[strKey]= value
     else:
-        print "unpackAlphanumericHisto: not alphanumeric ", hist.GetName()
+        print( "unpackAlphanumericHisto: not alphanumeric ", hist.GetName() )
     return values
 
 def getCutflow( ecm="161" ):
@@ -132,12 +125,12 @@ def getCutflow( ecm="161" ):
     return sampleCounters
 
 def getFromConfig( config, section, key ):
-    import ConfigParser
+    import configparser
     result= None
     try:
         result= config.getfloat( section, key )
-    except( ConfigParser.NoSectionError, ConfigParser.NoOptionError ):
-        #print "getFromConfig: section or key error", section, key
+    except( configparser.NoSectionError, configparser.NoOptionError ):
+        # print( "getFromConfig: section or key error", section, key )
         pass
     return result
 
@@ -146,18 +139,18 @@ def printCutflow( ecm="161" ):
     dataCounter= sampleCounters["Data"]
     lumi= dataCounter["lumi"]
     MCsamples= [ "Signal", "AltSignal", "BkgWWqqqq", "BkgWWllqq", "BkgWWeeqq" ]
-    print "Cutflow for ecm=", ecm
-    print "cut    ",
+    print( "Cutflow for ecm=", ecm )
+    print( "cut    ", end="" )
     for sample in [ "Data" ] + MCsamples:
         if sample in sampleCounters:
-            print "{:>9}".format( sample ),
-    print "{:>9}".format( "MC sum" )
+            print( "{:>10}".format( sample ), end="" )
+    print( "{:>10}".format( "MC sum" ) )
     cuts= [ "l2mh", "nch7", "costt09", "sprime", "wqqqq", "wqqln" ]
     if ecm == "91":
         cuts= [ "tkmh", "nch7", "costt09" ]
     for cut in cuts:
-        print "{:7}".format( cut ),
-        print "{:9.1f}".format( dataCounter[cut] ),
+        print( "{:7}".format( cut ), end="" )
+        print( "{:10.1f}".format( dataCounter[cut] ), end="" )
         mcsum= 0.0
         for sample in MCsamples:
             if sample in sampleCounters:
@@ -165,9 +158,9 @@ def printCutflow( ecm="161" ):
                 wgt= lumi*sampleCounter["xsec"]/sampleCounter["events"]
                 value= sampleCounter[cut] * wgt
                 if sample != "AltSignal":
-                    mcsum+= value                
-                print "{:9.1f}".format( value ),
-        print "{:9.1f}".format( mcsum )
+                    mcsum+= value
+                print( "{:10.1f}".format( value ), end="" )
+        print( "{:10.1f}".format( mcsum ) )
     return
 
 def printCutflows():
